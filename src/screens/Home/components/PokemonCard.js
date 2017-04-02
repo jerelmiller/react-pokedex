8864099e2595d1087524ae277ae7139f20e72957
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 02:48:48 -0700

    Move theming into own component

--- src/screens/Home/components/PokemonCard.js
+++ src/screens/Home/components/PokemonCard.js
@@ -3,63 +3,8 @@ import { gql } from 'react-apollo'
 import CardLink from 'components/CardLink'
 import CardImage from 'components/CardImage'
 import CardInfo from 'components/CardInfo'
-import styled, { ThemeProvider } from 'styled-components'
-
-const themes = {
-  fire: {
-    primary: '#E44B4D',
-  },
-  grass: {
-    primary: '#4CAF50',
-  },
-  water: {
-    primary: '#2196F3',
-  },
-  bug: {
-    primary: '#009688',
-  },
-  normal: {
-    primary: '#4A4A4A',
-  },
-  poison: {
-    primary: 'rgb(139, 90, 139)',
-  },
-  electric: {
-    primary: 'rgb(246, 189, 32)',
-  },
-  ground: {
-    primary: 'rgb(172, 156, 98)',
-  },
-  fairy: {
-    primary: 'rgb(255, 172, 172)',
-  },
-  fighting: {
-    primary: 'rgb(148, 139, 123)',
-  },
-  psychic: {
-    primary: 'rgb(189, 156, 230)'
-  },
-  rock: {
-    primary: 'rgb(123, 115, 115)'
-  },
-  ghost: {
-    primary: '#E2E2E2',
-    text: '#4A4A4A'
-  },
-  ice: {
-    primary: '#81D4FA'
-  },
-  dragon: {
-    primary: '#5C6BC0'
-  }
-}
-
-const defaultTheme = {
-  primary: '#fff',
-  text: '#666'
-}
-
-const themeFromType = type => themes[type.toLowerCase()] || defaultTheme
+import PokemonTheme from 'components/PokemonTheme'
+import styled from 'styled-components'
 
 const PokemonInfo = styled(CardInfo)`
   background: ${({ theme }) => theme.primary}
@@ -84,7 +29,7 @@ const PokemonImageContainer = styled.div`
 `
 
 const PokemonCard = ({ pokemon }) => (
-  <ThemeProvider theme={ themeFromType(pokemon.types[0]) }>
+  <PokemonTheme type={ pokemon.types[0] }>
     <CardLink to={ `/pokemon/${pokemon.id}` }>
       <PokemonImageContainer>
         <CardImage src={ pokemon.image } alt={ pokemon.name } />
@@ -94,7 +39,7 @@ const PokemonCard = ({ pokemon }) => (
         <PokemonNumber>#{ pokemon.number }</PokemonNumber>
       </PokemonInfo>
     </CardLink>
-  </ThemeProvider>
+  </PokemonTheme>
 )
 
 PokemonCard.fragments = {
