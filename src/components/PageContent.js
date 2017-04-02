4d8b0142044497d1200027c2f059e6fd448a1d56
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 12:33:03 -0700

    Pull all info for a pokemon and display it

---
+++ src/components/PageContent.js
@@ -0,0 +1,9 @@
+import Container from './Container'
+import styled from 'styled-components'
+
+const PageContent = styled(Container)`
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+`
+
+export default PageContent
--- src/screens/Pokemon/index.js
+++ src/screens/Pokemon/index.js
@@ -1,7 +1,9 @@
-import Container from 'components/Container'
 import Header from './components/Header'
+import PageContent from 'components/PageContent'
+import PokemonCard from 'screens/Home/components/PokemonCard'
 import React, { PropTypes } from 'react'
 import Spinner from 'components/Spinner'
+import Grid from 'components/Grid'
 import { gql, graphql } from 'react-apollo'
 
 const Pokemon = ({ data: { loading, pokemon }}) => {
@@ -9,14 +11,61 @@ const Pokemon = ({ data: { loading, pokemon }}) => {
     return <Spinner />
   }
 
+  const {
+    classification,
+    name,
+    image,
+    types,
+    weight,
+    height,
+    resistant,
+    evolutions,
+    maxHP,
+    maxCP,
+    weaknesses,
+    attacks
+  } = pokemon
+
   return (
     <div>
-      <Header pokemonType={ pokemon.types[0] }>
-        { pokemon.name }
+      <Header pokemonType={ types[0] }>
+        { name }
       </Header>
-      <Container>
-        <img src={ pokemon.image } alt={ pokemon.name } />
-      </Container>
+      <PageContent>
+        <img src={ image } alt={ name } />
+        { types.join(', ') }
+        <dl>
+          <dt>Weight (min)</dt>
+          <dd>{ weight.minimum }</dd>
+          <dt>Weight (max)</dt>
+          <dd>{ weight.maximum }</dd>
+          <dt>Height (min)</dt>
+          <dd>{ height.minimum }</dd>
+          <dt>Height (max)</dt>
+          <dd>{ height.maximum }</dd>
+          <dt>Classification</dt>
+          <dd>{ classification }</dd>
+          <dt>Resistant</dt>
+          <dd>{ resistant.join(', ') }</dd>
+          <dt>Max HP</dt>
+          <dd>{ maxHP }</dd>
+          <dt>Max CP</dt>
+          <dd>{ maxCP }</dd>
+          <dt>Weaknesses</dt>
+          <dd>{ weaknesses.join(', ') }</dd>
+          <dt>Fast Attacks</dt>
+          <dd>{ attacks.fast.map(attack => attack.name).join(', ') }</dd>
+          <dt>Special Attacks</dt>
+          <dd>{ attacks.special.map(attack => attack.name).join(', ') }</dd>
+        </dl>
+
+        <h1>Evolutions</h1>
+        <Grid>
+          { (evolutions || []).map(pokemon =>
+            <PokemonCard key={ pokemon.id } pokemon={ pokemon } />
+          )}
+        </Grid>
+      </PageContent>
     </div>
   )
 }
@@ -31,11 +80,38 @@ Pokemon.propTypes = {
 const pokemonQuery = gql`
   query GetPokemon($id: String) {
     pokemon(id: $id) {
+      classification
       image
       name
       types
+      resistant
+      maxHP
+      maxCP
+      weaknesses
+      weight {
+        minimum
+        maximum
+      }
+      height {
+        minimum
+        maximum
+      }
+      evolutions {
+        id
+        ...PokemonCard
+      }
+      attacks {
+        fast {
+          name
+        }
+        special {
+          name
+        }
+      }
     }
   }
+
+  ${PokemonCard.fragments.pokemon}
 `
 
 export default graphql(pokemonQuery, {
