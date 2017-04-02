dd6ed517f66ab3ac440a120b4b02d83ef62f1995
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 02:17:16 -0700

    Add pokemon route and shell component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from 'screens/Home'
 import Pokemon from 'screens/Pokemon'
 import React  from 'react'
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 
 const App = () => (
   <div>
-    <Route exact path='/' component={ Home } />
+    <Route exact path='/' render={ () => <Redirect to='/pokemon' /> } />
+    <Route exact path='/pokemon' component={ Home } />
     <Route path='/pokemon/:id' component={ Pokemon } />
   </div>
 )
--- src/screens/Pokemon/index.js
+++ src/screens/Pokemon/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import Spinner from 'components/Spinner'
+import { gql, graphql } from 'react-apollo'
 
-const Pokemon = () => (
-  <div>Pokemon</div>
-)
+const Pokemon = ({ data: { loading, pokemon }}) => {
+  if (loading) {
+    return <Spinner />
+  }
 
-export default Pokemon
+  return (
+    <div>{ pokemon.name }</div>
+  )
+}
+
+Pokemon.propTypes = {
+  data: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    pokemon: PropTypes.object
+  }).isRequired
+}
+
+const pokemonQuery = gql`
+  query GetPokemon($id: String) {
+    pokemon(id: $id) {
+      name
+    }
+  }
+`
+
+export default graphql(pokemonQuery, {
+  options: ({ match }) => ({ variables: { id: match.params.id }})
+})(Pokemon)
