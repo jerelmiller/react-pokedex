743dac6bb8a7731b654cbff1c2534786d85d8986
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 01:22:04 -0700

    Move pokemon list to home component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import Grid from './components/Grid'
-import PokemonCard from './components/PokemonCard'
-import React, { PropTypes } from 'react'
-import Spinner from './components/Spinner'
-import { gql, graphql } from 'react-apollo'
+import Home from 'screens/Home'
+import React  from 'react'
+import { Route } from 'react-router-dom'
 
-const App = ({ data: { loading, pokemons }}) => {
-  if (loading) {
-    return <Spinner size={ 80 } />
-  }
+const App = () => (
+  <div>
+    <Route exactly path='/' component={ Home } />
+  </div>
+)
 
-  return (
-    <div style={{ width: '80%', margin: '0 auto' }}>
-      <Grid>
-        { pokemons.map(pokemon =>
-          <PokemonCard key={ pokemon.id } pokemon={ pokemon } />
-        )}
-      </Grid>
-    </div>
-  )
-}
-
-App.propTypes = {
-  data: PropTypes.shape({
-    loading: PropTypes.bool.isRequired,
-    pokemons: PropTypes.array
-  }).isRequired
-}
-
-export default graphql(gql`
-  query GetPokemons {
-    pokemons(first: 151) {
-      id
-      ...PokemonCard
-    }
-  }
-  ${PokemonCard.fragments.pokemon}
-`)(App)
+export default App
---
+++ src/screens/Home.js
@@ -0,0 +1,38 @@
+import Grid from 'components/Grid'
+import PokemonCard from 'components/PokemonCard'
+import React, { PropTypes } from 'react'
+import Spinner from 'components/Spinner'
+import { gql, graphql } from 'react-apollo'
+
+const Home = ({ data: { loading, pokemons }}) => {
+  if (loading) {
+    return <Spinner size={ 80 } />
+  }
+
+  return (
+    <div style={{ width: '80%', margin: '0 auto' }}>
+      <Grid>
+        { pokemons.map(pokemon =>
+          <PokemonCard key={ pokemon.id } pokemon={ pokemon } />
+        )}
+      </Grid>
+    </div>
+  )
+}
+
+Home.propTypes = {
+  data: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    pokemons: PropTypes.array
+  }).isRequired
+}
+
+export default graphql(gql`
+  query GetPokemons {
+    pokemons(first: 151) {
+      id
+      ...PokemonCard
+    }
+  }
+  ${PokemonCard.fragments.pokemon}
+`)(Home)
