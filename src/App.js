1f1a8c227bcfdeb4e62776fddc74404a88616369
Jerel Miller <jerelmiller@gmail.com>
2017-04-01 21:06:33 -0700

    Add some default app code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { gql, graphql } from 'react-apollo'
 import './App.css'
 
-const App = () => (
-  <div>Hello Pokemon</div>
-)
+const App = ({ loading, data: { pokemons = [] }}) => {
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
-export default App
+  return (
+    <div>
+      { pokemons.map(pokemon =>
+        <div key={ pokemon.number }>
+          { pokemon.number } { pokemon.name }
+          <img src={ pokemon.image } alt={ pokemon.name } />
+        </div>
+      )}
+    </div>
+  )
+}
+
+App.propTypes = {
+  data: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    pokemons: PropTypes.array
+  }).isRequired
+}
+
+export default graphql(gql`
+  query GetPokemons {
+    pokemons(first: 10) {
+      number
+      name
+      image
+    }
+  }
+`)(App)
