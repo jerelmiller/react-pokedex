8251afa62228c1ce10b06c5ef536bd87136766fe
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 00:43:16 -0700

    Add hover effect

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,18 @@ import GridItem from './GridItem'
 const Card = styled(GridItem)`
   display: flex;
   flex-direction: column;
-  flex: 1 23%;
+  flex: 1 24%;
   background: #fff;
-	box-shadow: 0px 0px 1px 0px rgba(0,0,0,0.25);
-  margin: 1%;
+  box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.25);
+  margin: 0.5%;
   border-radius: 3px;
+  transition: all 0.15s ease-out;
+
+  &:hover {
+    transform: scale(1.03);
+    cursor: pointer;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);
+  }
 `
 
 export default Card
