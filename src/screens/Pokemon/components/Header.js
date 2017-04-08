929e4f21f1f9b7e04ea99c29363bcce03ab11373
Jerel Miller <jerelmiller@gmail.com>
2017-04-08 03:43:31 -0700

    Replace back arrow with close button

--- src/screens/Pokemon/components/Header.js
+++ src/screens/Pokemon/components/Header.js
@@ -1,4 +1,3 @@
-import LeftArrow from 'components/LeftArrow'
 import PokemonTheme, { themeFromType } from 'components/PokemonTheme'
 import React from 'react'
 import styled from 'styled-components'
@@ -41,35 +40,25 @@ const Heading = styled.h2`
   text-align: center;
 `
 
-const BackButton = styled(Link)`
+const CloseButton = styled(Link)`
   color: ${({ theme }) => theme.text}
   position: absolute;
-  left: 1.5rem;
-  top: 0.75rem;
-  font-weight: 700;
-  padding-right: 1.5rem;
-  border-right: 1px solid ${({ theme }) => theme.text}
-  line-height: 1.5rem;
-  height: 3rem;
+  right: 1.5rem;
+  top: 1.5rem;
+  font-weight: 300;
+  line-height: 1rem;
   display: block;
-
-  svg {
-    position: relative;
-    top: 50%;
-    transform: translateY(-50%);
-  }
+  font-size: 3rem;
+  opacity: 0.5;
 `
 
 const Header = ({ children, pokemonType }) => (
   <PokemonTheme type={ pokemonType }>
     <ThemedHeader>
-      <BackButton to='/pokemon'>
-        <LeftArrow
-          size='2rem'
-          color={ themeFromType(pokemonType).text }
-        />
-      </BackButton>
       <Heading>{children}</Heading>
+      <CloseButton to='/pokemon'>
+        &times;
+      </CloseButton>
     </ThemedHeader>
   </PokemonTheme>
 )
