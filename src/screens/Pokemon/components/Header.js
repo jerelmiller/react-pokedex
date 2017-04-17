062ff60a472c06db3ffcbcfd94ce2b972d4490f1
Jerel Miller <jerelmiller@gmail.com>
2017-04-16 19:26:47 -0700

    Remove unused variable

--- src/screens/Pokemon/components/Header.js
+++ src/screens/Pokemon/components/Header.js
@@ -1,4 +1,4 @@
-import PokemonTheme, { themeFromType } from 'components/PokemonTheme'
+import PokemonTheme from 'components/PokemonTheme'
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
