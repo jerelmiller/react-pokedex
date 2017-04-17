403e658752dda3f821ee54d30ad3c69bcd9dfe66
Jerel Miller <jerelmiller@gmail.com>
2017-04-16 20:30:25 -0700

    Add header for main page

---
+++ src/screens/Pokemons/components/Header.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export default styled.header`
+  box-shadow: 0px 0px 1px 1px #ddd;
+  background-color: white;
+  padding: 0.75rem 0;
+  margin-bottom: 1rem;
+  position: relative;
+
+  svg {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  h1 {
+    display: inline-block;
+    vertical-align: middle;
+    margin-left: 0.75rem;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+`
--- src/screens/Pokemons/index.js
+++ src/screens/Pokemons/index.js
@@ -1,5 +1,7 @@
 import Container from 'components/Container'
 import Grid from 'components/Grid'
+import Header from './components/Header'
+import Pokeball from 'components/Pokeball'
 import PokemonCard from 'screens/shared/PokemonCard'
 import React, { PropTypes } from 'react'
 import Spinner from 'components/Spinner'
@@ -11,13 +13,21 @@ const Pokemons = ({ data: { loading, pokemons }}) => {
   }
 
   return (
-    <Container>
-      <Grid>
-        { pokemons.map(pokemon =>
-          <PokemonCard key={ pokemon.id } pokemon={ pokemon } />
-        )}
-      </Grid>
-    </Container>
+    <div>
+      <Header>
+        <Container>
+          <Pokeball size={ 40 } />
+          <h1>React Pokedex</h1>
+        </Container>
+      </Header>
+      <Container>
+        <Grid>
+          { pokemons.map(pokemon =>
+            <PokemonCard key={ pokemon.id } pokemon={ pokemon } />
+          )}
+        </Grid>
+      </Container>
+    </div>
   )
 }
 
