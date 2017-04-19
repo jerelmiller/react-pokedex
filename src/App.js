8722b08dc0dac799e794f9a1397478fc85987633
Jerel Miller <jerelmiller@gmail.com>
2017-04-19 00:12:39 -0700

    SOMETHING RENDERS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import Pokemons from 'screens/Pokemons'
-import Pokemon from 'screens/Pokemon'
+// import Pokemons from 'screens/Pokemons'
+// import Pokemon from 'screens/Pokemon'
 import React  from 'react'
 import environment from 'config/environment'
 import { Redirect, Route } from 'react-router-dom'
 import { QueryRenderer, graphql } from 'react-relay'
 
 const App = () =>
-  <div>Test</div>
-  // <QueryRenderer
-  //   environment={ environment }
-  //   query={ graphql`
-  //     query AppQuery {
-  //       pokemons {
-  //         ...index_pokemons
-  //       }
-  //     }
-  //   `}
-  // />
+  <QueryRenderer
+    environment={ environment }
+    query={ graphql`
+      query AppQuery {
+        pokemons {
+          ...index_pokemons
+        }
+      }
+    `}
+    render={ ({ error, props }) => {
+      return <div>Render me</div>
+    }}
+  />
   // <div>
   //   <Route exact path='/' render={ () => <Redirect to='/pokemon' /> } />
   //   <Route exact path='/pokemon' render={
