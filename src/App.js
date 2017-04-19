42dd54f242bbd62d167ec4b7ef9db2bb15b464d9
Jerel Miller <jerelmiller@gmail.com>
2017-04-19 01:23:39 -0700

    Fix some lint errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import environment from 'config/environment'
 import Pokemons from 'screens/Pokemons'
 import Pokemon from 'screens/Pokemon'
 import React  from 'react'
-import Spinner from 'components/Spinner'
 import { Redirect, Route } from 'react-router-dom'
-import { QueryRenderer, graphql } from 'react-relay'
 
 const App = () =>
   <div>
--- src/screens/shared/PokemonCard.js
+++ src/screens/shared/PokemonCard.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import CardLink from 'components/CardLink'
 import CardImage from 'components/CardImage'
 import CardInfo from 'components/CardInfo'
