816464468178d7afbcbd0e01cf8426c6ed13d4ea
Jerel Miller <jerelmiller@gmail.com>
2017-05-13 20:38:53 -0700

    Convert header to glamorous

--- src/screens/Pokemon/components/Header.js
+++ src/screens/Pokemon/components/Header.js
@@ -1,50 +1,57 @@
 import Theme from 'components/Theme'
 import React from 'react'
-import styled from 'styled-components'
+import glamorous from 'glamorous'
 import { Link } from 'react-router-dom'
 
 const ballSize = 2
 
-const ThemedHeader = styled.header`
-  background: ${({ theme }) => theme.primary}
-  color: ${({ theme }) => theme.text}
-  position: relative;
-  height: 4.5rem;
+const ThemedHeader = glamorous.header(
+  {
+    position: 'relative',
+    height: '4.5rem',
+    '&::after, &::before': {
+      content: '',
+      borderRadius: '50%',
+      position: 'absolute',
+      left: '50%',
+      transform: 'translateX(-50%)'
+    },
+    '&::after': {
+      backgroundColor: '#f6f6f6',
+      width: `${ballSize - 0.5}rem`,
+      height: `${ballSize - 0.5}rem`,
+      bottom: `-${(ballSize - 0.5) / 2}rem`
+    },
+    '&::before': {
+      width: `${ballSize}rem`,
+      height: `${ballSize}rem`,
+      bottom: `-${ballSize / 2}rem`,
+    },
+  },
+  (_, { primary, text }) => ({
+    color: text,
+    background: primary,
+    '&::before': {
+      backgroundColor: primary
+    }
+  })
+)
 
-  &:after, &:before {
-    content: '';
-    border-radius: 50%;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-
-  &:after {
-    background-color: #f6f6f6;
-    width: ${ballSize - 0.5}rem;
-    height: ${ballSize - 0.5}rem;
-    bottom: -${(ballSize - 0.5) / 2}rem;
-  }
-
-  &:before {
-    background-color: ${({ theme }) => theme.primary}
-    width: ${ballSize}rem;
-    height: ${ballSize}rem;
-    bottom: -${ballSize / 2}rem;
-  }
-`
-
-const CloseButton = styled(Link)`
-  color: ${({ theme }) => theme.text}
-  position: absolute;
-  top: 1.5rem;
-  right: 1.5rem;
-  font-weight: 300;
-  line-height: 1rem;
-  display: block;
-  font-size: 3rem;
-  opacity: 0.5;
-`
+const CloseButton = glamorous(Link)(
+  {
+    position: 'absolute',
+    top: '1.5rem',
+    right: '1.5rem',
+    fontWeight: '300',
+    lineHeight: '1rem',
+    display: 'block',
+    fontSize: '3rem',
+    opacity: '0.5'
+  },
+  (_, { text }) => ({
+    color: text
+  })
+)
 
 const Header = ({ children, pokemonType }) => (
   <Theme type={ pokemonType }>
