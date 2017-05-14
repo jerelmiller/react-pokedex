c2b2f0f1c795a5b53d1ecef9cceecea526ef8c24
Jerel Miller <jerelmiller@gmail.com>
2017-05-13 19:16:22 -0700

    Switch some components over to glamorous

--- src/screens/shared/PokemonCard.js
+++ src/screens/shared/PokemonCard.js
@@ -5,28 +5,29 @@ import CardImage from 'components/CardImage'
 import CardInfo from 'components/CardInfo'
 import Theme from 'components/Theme'
 import styled from 'styled-components'
+import glamorous from 'glamorous'
 
 const PokemonInfo = styled(CardInfo)`
   background: ${({ theme }) => theme.primary}
   color: ${({ theme } ) => theme.text || '#fff'}
 `
 
-const PokemonName = styled.h3`
-  text-align: center;
-  margin-top: 0;
-  margin-bottom: 0.5rem;
-`
+const PokemonName = glamorous.h3({
+  textAlign: 'center',
+  marginTop: 0,
+  marginBottom: '0.5rem'
+})
 
-const PokemonNumber = styled.div`
-  text-align: center;
-  font-weight: 300;
-`
+const PokemonNumber = glamorous.div({
+  textAlign: 'center',
+  fontWeight: 300
+})
 
-const PokemonImageContainer = styled.div`
-  padding: 1rem;
-  margin-top: auto;
-  align-self: center;
-`
+const PokemonImageContainer = glamorous.div({
+  padding: '1rem',
+  marginTop: 'auto',
+  alignSelf: 'center'
+})
 
 const PokemonCard = ({ pokemon }) => (
   <Theme type={ pokemon.types[0] }>
