3d4627d6cc8f61c5f7e677a96c4e45c1e02376a9
Jerel Miller <jerelmiller@gmail.com>
2017-05-13 19:40:12 -0700

    Convert header to glamorous

--- src/screens/Pokemons/components/Header.js
+++ src/screens/Pokemons/components/Header.js
@@ -1,22 +1,22 @@
-import styled from 'styled-components'
+import glamorous from 'glamorous'
 
-export default styled.header`
-  box-shadow: 0px 0px 1px 1px #ddd;
-  background-color: white;
-  padding: 0.75rem 0;
-  margin-bottom: 1rem;
-  position: relative;
+export default glamorous.header({
+  boxShadow: '0px 0px 1px 1px #ddd',
+  backgroundColor: 'white',
+  padding: '0.75rem 0',
+  marginBottom: '1rem',
+  position: 'relative',
 
-  svg {
-    display: inline-block;
-    vertical-align: middle;
+  ' svg': {
+    display: 'inline-block',
+    verticalAlign: 'middle'
+  },
+
+  ' h1': {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    marginLeft: '0.75rem',
+    marginTop: '0',
+    marginBottom: '0'
   }
-
-  h1 {
-    display: inline-block;
-    vertical-align: middle;
-    margin-left: 0.75rem;
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-`
+})
