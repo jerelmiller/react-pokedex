31848147e8df59b77e50bb97fe1a1807d64fae47
Jerel Miller <jerelmiller@gmail.com>
2017-05-13 19:55:39 -0700

    Convert Card to glamorous

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,20 @@
-import styled from 'styled-components'
+import glamorous from 'glamorous'
 
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex: 0 24%;
-  background: #fff;
-  box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.25);
-  margin: 0.5%;
-  border-radius: 3px;
-  transition: all 0.15s ease-out;
+const Card = glamorous.div({
+  display: 'flex',
+  flexDirection: 'column',
+  flex: '0 24%',
+  background: '#fff',
+  boxShadow: '0px 0px 1px 0px rgba(0, 0, 0, 0.25)',
+  margin: '0.5%',
+  borderRadius: '3px',
+  transition: 'all 0.15s ease-out',
 
-  &:hover {
-    transform: scale(1.03);
-    cursor: pointer;
-    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);
+  ':hover': {
+    transform: 'scale(1.03)',
+    cursor: 'pointer',
+    boxShadow: '0px 0px 5px 0px rgba(0, 0, 0, 0.25)'
   }
-`
+})
 
 export default Card
