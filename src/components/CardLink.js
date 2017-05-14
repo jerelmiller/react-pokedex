d70bae59cb7c3e135a9c0314f6b9e631f81fe711
Jerel Miller <jerelmiller@gmail.com>
2017-05-13 20:00:13 -0700

    Convert CardLink to glamorous

--- src/components/CardLink.js
+++ src/components/CardLink.js
@@ -1,22 +1,22 @@
-import styled from 'styled-components'
+import glamorous from 'glamorous'
 import { Link } from 'react-router-dom'
 
-const CardLink = styled(Link)`
-  display: flex;
-  flex-direction: column;
-  flex: 0 24%;
-  background: #fff;
-  box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.25);
-  margin: 0.5%;
-  border-radius: 3px;
-  transition: all 0.15s ease-out;
-  text-decoration: none;
+const CardLink = glamorous(Link)({
+  display: 'flex',
+  flexDirection: 'column',
+  flex: '0 24%',
+  background: '#fff',
+  boxShadow: '0px 0px 1px 0px rgba(0, 0, 0, 0.25)',
+  margin: '0.5%',
+  borderRadius: '3px',
+  transition: 'all 0.15s ease-out',
+  textDecoration: 'none',
 
-  &:hover {
-    transform: scale(1.03);
-    cursor: pointer;
-    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);
+  ':hover': {
+    transform: 'scale(1.03)',
+    cursor: 'pointer',
+    boxShadow: '0px 0px 5px 0px rgba(0, 0, 0, 0.25)'
   }
-`
+})
 
 export default CardLink
